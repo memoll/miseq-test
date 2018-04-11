734f7cc52521638bfde276eeb7c5901291602d1a
mona <mona@phyllo.ddns.uqam.ca>
2018-04-10 21:14:05 -0400

    update files

--- data-preparation.R
+++ data-preparation.R
@@ -1,11 +1,94 @@
-## Mona Parizadeh - January 2018
+## Data preparation for analysis with DADA2
+## Mona Parizadeh - March 2018
 
-## Data preparation for DADA2
+# As mentioned in DADA2 tutorial, for the analysis we'll need demultiplexedpaired-end sequences in fastq format that have been split (or “demultiplexed”) by sample and also the barcodes/adapters have already been removed.
 
-### As mentioned in DADA2 tutorial, for the analysis we'll need paired-end sequences in fastq format that have been split (or “demultiplexed”) by sample and also the barcodes/adapters have already been removed.
-### If you are using barcodes with the same length, we may want to use "extract_barcodes.py" and "split_libraries_fastq.py" or "split_libraries.py" from Qiime1
-### "split_libraries_fastq.py" doesn't have an option to extract various length barcodes, in that case, we'll need to convert fastq files to fasta and qual files and then use "split_libraries.py".
+## 1. Trim adapters - bbduk ####
+#/data/apps/bbmap/bbduk.sh –Xmx1g in=file_R#_001.fastq out=adapter-trimmed/file-trimmed_R#.fastq ref=/data/apps/bbmap/resources/adapters.fa ktrim=r k=23 mink=11 hdist=1 tbo tpe
 
-## Split fastq files to fasta and qual
-### Convert trimmed sequences format from fastq to fasta and qual
-python /data/shared/scripts/fastq_to_fasta_qual.py $file-trimmed
+
+## 2. Make mapping_file.tsv - excel #### 
+# #SampleID - BarcodeSequence	- LinkerPrimerSequence -	Barcode	- ReversePrimer	- Description
+
+# Problem: in our Miseq SOP we're using barcodes w/ various lengths
+# Strategy: making fake barcodes, by adding a part of the primer to the end of each barcode
+
+# 3. Write the mapping_file.txt - R ####
+mapping_file <- read.table("miseq-test-tables/miseq-run-test-2016 - mapping-file.tsv")
+colnames(mapping_file) <- (c("#SampleID", "BarcodeSequence", "LinkerPrimerSequence", "Barcode", "ReversePrimer", "Description"))
+# Add primers to barcodes and cut them to the length of the longest barcode
+mapping_file$BarcodeSequence <- strtrim(paste0(mapping_file$BarcodeSequence,mapping_file$LinkerPrimerSequence),max(nchar(as.character(mapping_file$BarcodeSequence)))); nchar(mapping_file$BarcodeSequence) # for BarcodeSequence (F)
+mapping_file$Barcode <- strtrim(paste0(mapping_file$Barcode,mapping_file$Reverse), max(nchar(as.character(mapping_file$Barcode)))); nchar(mapping_file$Barcode) # for Barcode (R)
+# Put new barcodes together and replace all the characters of the barcodes other than ATCG with "."
+mapping_file$BarcodeSequence <- gsub("[^ATCG]",".", paste0(mapping_file$BarcodeSequence,mapping_file$Barcode)) 
+mapping_file$Barcode <- NULL # clear the barcode (R) column
+#mapping_file
+write.table(mapping_file, "miseq-test-tables/mapping-miseq-run-test-2016.txt", sep = "\t", row.names = F, quote = F)
+
+# 4. Validate the mapping file - qiime ####
+# validate_mapping_file.py -m miseq-test-tables/mapping-miseq-run-test-2016.txt -o validate-mapping
+
+# 5. Make mapping file - idemp (barcode tab identifier) ####
+idemp_mapping_file <- mapping_file[,c(2,1)]; colnames(idemp_mapping_file) <- NULL
+write.table(idemp_mapping_file, "miseq-test-tables/idemp-mapping-file.txt", sep = "\t", row.names = F, quote = F)
+
+# 6. Extract barcodes - qiime ####
+# Based of out sequencing protocol, R1 is reverse and R2 is forward; so for this step bc1 = R -> --bc1_len = 17 and bc2 = F -> --bc2_len = 14
+# extract_barcodes.py -f rawdata/adapter-trimmed/Mona-run-test_S1_L001-trimmed_R2.fastq -r rawdata/adapter-trimmed/Mona-run-test_S1_L001-trimmed_R1.fastq -c barcode_paired_end -m miseq-test-tables/mapping-miseq-run-test-2016.txt --attempt_read_reorientation --bc1_len 17 --bc2_len 14 -o dada2-analysis/processed_seqs
+
+# 7. Trim the rest of the primers from reads1 and reads2 - :####
+
+# @param id : identifier
+# @param seq.read : character vector containing reads
+# @param seq.qual : character vector containing quality
+# @return
+fastq_file <- read_file("reads1.1000.fastq")
+cut_left <- function(fastq_file){
+  seq.read <- subseq(fastq_file ((1:length(fastq_file) %% 4 + 1) %in% c(2,4)), start=20, end=nchar(fastq_file))
+}
+
+
+library(Biostrings)
+fastq_file <- readDNAStringSet("reads1.1000.fastq", format("fastq"))
+fastq_file_19 <- subseq(fastq_file, start=20, end=nchar(fastq_file))
+fastq_file_19[246]
+
+write(fastq_file_19, "", sep = "\n")
+
+
+#install.packages("readr")
+library(readr)
+#library(tools)
+fastq_file <- "reads1.fastq"
+fastq_data <- read_lines(fastq_file, skip = 0); length(fastq_file)
+sel_line <- (1:length(fastq_data) %% 4 + 1) %in% c(2,4) # selects the lines that we need to manipulate, 2: reads, 4: quality.
+trim_size <- max(nchar(as.character(mapping_file$LinkerPrimerSequence)))
+prm_trimmed <- ifelse(sel_line, substring(fastq_data[10], trim_size + 1, nchar(fastq_data)), fastq_data) # removes the beginning of each read and quality line and keep fromn the max length of the primer to the end of the read and quality lines
+write_lines(prm_trimmed, paste0(file_path_sans_ext(fastq_file)),".prm-trimmed.fastq")
+
+
+# sp = split(x, line_nb)
+# read <- substring(sp$'2', 31, nchar(sp$'2'))
+# qual <- substring(sp$'4', 31, nchar(sp$'4'))
+#substring(sp$`2`,30,length(sp$`2`))
+#read <- sapply(strsplit(sp$'2',substring(sp$'2', 1, 30) ,2), `[`, 2)
+#qual <- sapply(strsplit(sp$'4',substring(sp$'4', 1, 30) ,2), `[`, 2)
+#table(read==read2)
+
+
+
+# 8. Demultiplex - idemp:####
+#idemp -b idemp-mapping-file.txt -I1 processed_seqs/barcodes.fastq -R1 processed_seqs/reads2.fastq -R2 processed_seqs/reads1.fastq -m 1 -o idemp-demultiplexed
+
+# 12. Verify the number of demultiplexed samples files (Number of files in idemp-demultiplexed - 2)/2 :
+# Number of files: cd idemp-demultiplexed > ls -1 | wc -l     
+
+
+
+
+
+
+
+
+
+
--- mapping-file.R
+++ mapping-file.R
@@ -1,34 +1,30 @@
 ## Mona Parizadeh - March 2018
-<<<<<<< HEAD
-
-## How to prepare mapping file?
-=======
-
 # How to prepare mapping file?
 
 # 1. In excel: #SampleID - BarcodeSequence	- LinkerPrimerSequence -	Barcode	- ReversePrimer	- Description; save as .tsv ####
 
 # 2. Import mapping_file.tsv in R:####
-mapping_file <- read.table("Google Drive/mapping-miseq-run-test-2016.tsv")
+mapping_file <- read.table("miseq-test-tables/miseq-run-test-2016 - mapping-file.tsv")
 colnames(mapping_file) <- (c("#SampleID", "BarcodeSequence", "LinkerPrimerSequence", "Barcode", "ReversePrimer", "Description"))
-mapping_file
+#mapping_file
 
 # Verify the length of the longest barcode - we'll need it to make fake barcodes later
-max_BarcodeSequence <- max(nchar(as.character(mapping_file$BarcodeSequence)))
-max_Barcode <- max(nchar(as.character(mapping_file$Barcode)))
+max_BC_seq <- max(nchar(as.character(mapping_file$BarcodeSequence)))
+max_BC <- max(nchar(as.character(mapping_file$Barcode)))
 
 # 3. Add primers to the end of each barcode sequence - in order to make fake barcodes with the same length later ####
-BarcodeSequence_primer <-  paste0(mapping_file$BarcodeSequence,mapping_file$LinkerPrimerSequence)
-mapping_file$BarcodeSequence <- BarcodeSequence_primer
-Barcode_primer <- paste0(mapping_file$Barcode,mapping_file$Reverse)
-mapping_file$Barcode <- Barcode_primer
+#BC_seq_prm <-  paste0(mapping_file$BarcodeSequence,mapping_file$LinkerPrimerSequence)
+#mapping_file$BarcodeSequence <- BC_seq_prm
+mapping_file$BarcodeSequence <-  paste0(mapping_file$BarcodeSequence,mapping_file$LinkerPrimerSequence)
+#BC_prm <- paste0(mapping_file$Barcode,mapping_file$Reverse)
+#mapping_file$Barcode <- BC_prm
+mapping_file$Barcode <- paste0(mapping_file$Barcode,mapping_file$Reverse)
 
-# 4. Cut all the new barcodes to the same length -length of the longest barcode ####
+# 4. Cut all the new barcodes to the same length -length of the longest barcode-primer ####
 
-# length of the shortest barcode
 # BarcodeSequence column:
 #nchar(mapping_file$BarcodeSequence)
-BarcodeSequence_trimmed <- strtrim(mapping_file$BarcodeSequence,max_BarcodeSequence)
+BC_seq_trimmed <- strtrim(mapping_file$BarcodeSequence,max_BarcodeSequence)
 nchar(BarcodeSequence_trimmed)
 mapping_file$BarcodeSequence <- BarcodeSequence_trimmed
 
@@ -53,10 +49,10 @@ mapping_file
 write.table(mapping_file, "R_projects/miseq-test/mapping-miseq-run-test-2016.txt", sep = "\t", row.names = F, quote = F)
 
 # 7. Validate the mapping file - qiime:####
-#In Terminal: validate_mapping_file.py -m mapping-miseq-run-test-2016.txt -o validate-mapping
+#bash: validate_mapping_file.py -m mapping-miseq-run-test-2016.txt -o validate-mapping
 
 # check if there are reverses of each barcodes of F and R in R1 and R2, and for the paired barcodes as well (reversed and not reversed)
-# In R: reverse("ACAGCCACCCATCGAAAGAGCAACATCCTAA")
+# In R: reverse("ACAGCCACCCATCGAAAGAGCAACATCCTAA") and grep it in R1 and R2 - bash
 
 # 8. Make mapping file for idemp - barcode tab identifier:####
 idemp_mapping_file <- mapping_file[,c(2,1)]
@@ -64,59 +60,50 @@ colnames(idemp_mapping_file) <- NULL
 write.table(idemp_mapping_file, "R_projects/miseq-test/idemp-mapping-file.txt", sep = "\t", row.names = F, quote = F)
 
 # 9. Extract barcodes - qiime:####
-# In terminal: extract_barcodes.py -f rawdata/adapter-trimmed-new/Mona-run-test_S1_L001-trimmed_R1.fastq -r rawdata/adapter-trimmed-new/Mona-run-test_S1_L001-trimmed_R2.fastq -c barcode_paired_end -m mapping-miseq-run-test-2016.txt --attempt_read_reorientation --bc1_len 17 --bc2_len 14 -o processed_seqs
+# bash: extract_barcodes.py -f rawdata/adapter-trimmed/Mona-run-test_S1_L001-trimmed_R2.fastq -r rawdata/adapter-trimmed/Mona-run-test_S1_L001-trimmed_R1.fastq -c barcode_paired_end -m mapping-miseq-run-test-2016.txt --attempt_read_reorientation --bc1_len 17 --bc2_len 14 -o processed_seqs
+# Based of out sequencing protocol, R1 is reverse and R2 is forward; so for this step bc1 = R -> --bc1_len = 17 and bc2 = F -> --bc2_len = 14
 
-# 10. Demultiplex - idemp:####
-#idemp -b idemp-mapping-file.txt -I1 processed_seqs/barcodes.fastq -R1 processed_seqs/reads1.fastq -R2 processed_seqs/reads2.fastq -m 1 -o idemp-demultiplexed
+# 10. Trim the rest of primers from reads1 and reads2 - :####
+# bash: 
+# /data/apps/bbmap/bbduk.sh in=processed_seqs/reads1.fastq out=processed_seqs/primer-trimmed/reads1.primerTrimmed.fastq ftl=36
+# reads1 : 18740998 - 18717004 = 23994
+# ftl = 30 -> 18717239
+# /data/apps/bbmap/bbduk.sh in=processed_seqs/reads2.fastq out=processed_seqs/primer-trimmed/reads2.primerTrimmed.fastq ftl=30
+# reads2 : 18740998 - 18717327 = 23671
+#ftl=36: 18717176
 
 
->>>>>>> 16221506ad790927d3e15e63484c78fa28d65b79
+#install.packages("readr")
+library(readr)
 
-#### 1. In excel: #SampleID - BarcodeSequence	- LinkerPrimerSequence -	Barcode	- Reverse	- Description; save as .tsv
+fastq_file = "/data/users/mona/miseq-test-2016/dada2-analysis/processed_seqs/reads1.fastq"
+x = read_lines(fastq_file, skip = 0); length(x)
+sel_line = (1:length(x) %% 4 + 1) %in% c(2,4) #selects the lines that we need to manipulate, 2: reada, 4: quality.
+x2 = ifelse(sel_line , substring(x, 37, nchar(x)), x)
+write_lines(x2,paste0(fastq_file,".trim30"))
 
-#### 2. Import mapping_file.tsv in R:
-mapping_file <- read.table("Google Drive/mapping-miseq-run-test-2016.tsv")
-colnames(mapping_file) <- (c("#SampleID", "BarcodeSequence", "LinkerPrimerSequence", "Barcode", "Reverse", "Description"))
-mapping_file
+# sp = split(x, line_nb)
+# read <- substring(sp$'2', 31, nchar(sp$'2'))
+# qual <- substring(sp$'4', 31, nchar(sp$'4'))
+#substring(sp$`2`,30,length(sp$`2`))
+#read <- sapply(strsplit(sp$'2',substring(sp$'2', 1, 30) ,2), `[`, 2)
+#qual <- sapply(strsplit(sp$'4',substring(sp$'4', 1, 30) ,2), `[`, 2)
+#table(read==read2)
 
-##### Verify the length of the longest barcode (we'll need it to make fake barcodes later)
-max_BarcodeSequence <- max(nchar(as.character(mapping_file$BarcodeSequence)))
-max_Barcode <- max(nchar(as.character(mapping_file$Barcode)))
 
-#### 3. Add primers to the end of each barcode sequence (in order to make fake barcodes with the same length later)
-BarcodeSequence_primer <-  paste0(mapping_file$BarcodeSequence,mapping_file$LinkerPrimerSequence)
-mapping_file$BarcodeSequence <- BarcodeSequence_primer
-Barcode_primer <- paste0(mapping_file$Barcode,mapping_file$Reverse)
-mapping_file$Barcode <- Barcode_primer
 
-#### 4. Cut all the new barcodes to the same length (length of the longest barcode)
+# 11. Demultiplex - idemp:####
+#idemp -b idemp-mapping-file.txt -I1 processed_seqs/barcodes.fastq -R1 processed_seqs/reads2.fastq -R2 processed_seqs/reads1.fastq -m 1 -o idemp-demultiplexed
 
-##### length of the shortest barcode
-#### BarcodeSequence column:
-#nchar(mapping_file$BarcodeSequence)
-BarcodeSequence_trimmed <- strtrim(mapping_file$BarcodeSequence,max_BarcodeSequence)
-nchar(BarcodeSequence_trimmed)
-mapping_file$BarcodeSequence <- BarcodeSequence_trimmed
+# 12. Verify the number of demultiplexed samples files (Number of files in idemp-demultiplexed - 2)/2 :
+# Number of files: cd idemp-demultiplexed > ls -1 | wc -l     
 
-##### Barcode column:
-#nchar(mapping_file$Barcode)
-Barcode_trimmed <- strtrim(mapping_file$Barcode,max_Barcode)
-nchar(Barcode_trimmed)
-mapping_file$Barcode <- Barcode_trimmed
 
-#### 5. Add Barcode (reverse/r2) to BarcodeSequence (forward/r2)
-BarcodeSequence_r1_r2 <- paste0(mapping_file$BarcodeSequence,mapping_file$Barcode)
-mapping_file$BarcodeSequence <- BarcodeSequence_r1_r2
 
-##### Remove the Barcode (reverse/r2) column
-mapping_file$Barcode <- NULL
-mapping_file
 
-#### 6. Export the mapping_file as a .txt file
-write.table(mapping_file, "R_projects/miseq-test/mapping-miseq-run-test-2016.txt", sep = "\t", row.names = F, quote = F)
 
-#### 7. Validate the mapping file (qiime):
-#In Terminal: validate_mapping_file.py -m mapping-miseq-run-test-2016.txt -o validate-mapping
 
-#check if you need to replace M,K,etc in barcodes with "."
+
+
+
 
